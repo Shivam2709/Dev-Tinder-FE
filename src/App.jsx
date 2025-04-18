6c21ead5bd186d49a68df3875ad1064f079b96ec
Shivam <shivamthakur2709@gmail.com>
2025-04-18 17:16:06 +0530

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
+      <Navbar />
       <h1 className="text-3xl font-extralight text-amber-400">Vite + React</h1>
     </>
   );
